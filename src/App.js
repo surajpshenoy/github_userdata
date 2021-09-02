531e85378f317f132c51d1b5c40d98d1b16827be
surajpshenoy <surajpshenoy@gmail.com>
2021-09-02 17:37:43 +1000

    Fetch user data and display

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
+
+
+const url = `https://api.github.com/users `;
 
 function App() {
+
+  const [users, setUsers] = useState([]);
+
+  const getData = async() => {
+    const response = await fetch(url);
+
+    const users = await response.json();
+    setUsers(users)
+  }
+
+  useEffect (()=> {
+
+    getData();
+
+  }, [])
+
   return (
     <div className="App">
-        <h4>GitHub user data</h4>
+        <h2>GitHub user data</h2>
+        <ul className="users">
+            {users.map((user) => {
+              const {id, login, avatar_url, html_url} = user;
+              return (
+                <li key={id}>
+                  <img src={avatar_url} alt ={login}/>
+                </li>
+              )
+            })}
+        </ul>
+
     </div>
   );
 }
