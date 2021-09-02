717fc961770cba23349e163faf5de2143c5a15d2
surajpshenoy <surajpshenoy@gmail.com>
2021-09-02 17:45:13 +1000

    Display users with data

--- src/App.js
+++ src/App.js
@@ -1,40 +1,39 @@
-import React, { useState, useEffect } from "react"
-
+import React, { useState, useEffect } from "react";
 
 const url = `https://api.github.com/users `;
 
 function App() {
-
   const [users, setUsers] = useState([]);
 
-  const getData = async() => {
+  const getData = async () => {
     const response = await fetch(url);
 
     const users = await response.json();
-    setUsers(users)
-  }
+    setUsers(users);
+  };
 
-  useEffect (()=> {
-
+  useEffect(() => {
     getData();
-
-  }, [])
+  }, []);
 
   return (
-    <div className="App">
-        <h2>GitHub user data</h2>
-        <ul className="users">
-            {users.map((user) => {
-              const {id, login, avatar_url, html_url} = user;
-              return (
-                <li key={id}>
-                  <img src={avatar_url} alt ={login}/>
-                </li>
-              )
-            })}
-        </ul>
-
-    </div>
+    <>
+      <h3>GitHub Users</h3>
+      <ul className="users">
+        {users.map((user) => {
+          const { id, login, avatar_url, html_url } = user;
+          return (
+            <li key={id}>
+              <img src={avatar_url} alt={login} />
+              <div>
+                <h4>{login}</h4>
+                <a href={html_url}>profile</a>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </>
   );
 }
 
